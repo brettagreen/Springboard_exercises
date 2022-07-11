e9d901ce201a28427a33f78e6ccdf144a5c0489e
Brett Green <brettagreen@users.noreply.github.com>
2022-07-11 13:46:25 -0500

    restAndSpread!

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -1,5 +1,5 @@
 const mine = arr => arr.map(val => val * 2);
-console.log(mine([2,4,6]));
+//console.log(mine([2,4,6]));
 
 const what = numbers => numbers.map(num => num **2).filter(square => square % 2 === 0);
-console.log(what([3,6,9]));
+//console.log(what([3,6,9]));
